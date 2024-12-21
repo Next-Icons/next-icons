4d476de027ba9ad663b41064d08ee7996ba39bcc
Alexandr Virgovič <91419390+devdeem@users.noreply.github.com>
2024-12-21 12:01:47 +0100

    0.0.4

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,49 @@
-import { ArrowDown } from "./";
+import { ArrowDown, ArrowDownSquare, ArrowLeft, ArrowLeftSquare, ArrowRight, ArrowRightSquare, ArrowUp, ArrowUpSquare } from "./";
 
-describe("ExampleComponent", () => {
+describe("ArrowDown Icon", () => {
 	it("is truthy", () => {
 		expect(ArrowDown).toBeTruthy();
+	});
+});
+
+describe("ArrowDownSquare Icon", () => {
+	it("is truthy", () => {
+		expect(ArrowDownSquare).toBeTruthy();
+	});
+});
+
+describe("ArrowLeft Icon", () => {
+	it("is truthy", () => {
+		expect(ArrowLeft).toBeTruthy();
+	});
+});
+
+describe("ArrowLeftSquare Icon", () => {
+	it("is truthy", () => {
+		expect(ArrowLeftSquare).toBeTruthy();
+	});
+});
+
+describe("ArrowRight Icon", () => {
+	it("is truthy", () => {
+		expect(ArrowRight).toBeTruthy();
+	});
+});
+
+describe("ArrowRightSquare Icon", () => {
+	it("is truthy", () => {
+		expect(ArrowRightSquare).toBeTruthy();
+	});
+});
+
+describe("ArrowUp Icon", () => {
+	it("is truthy", () => {
+		expect(ArrowUp).toBeTruthy();
+	});
+});
+
+describe("ArrowUpSquare Icon", () => {
+	it("is truthy", () => {
+		expect(ArrowUpSquare).toBeTruthy();
 	});
 });
